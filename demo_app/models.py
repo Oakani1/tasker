c9f6605e06b2a2a0f8f36520db21967acb6b23e2
Oluwole Akani <oakani@gmail.com>
2024-08-02 10:31:38 +0000

    add entry form for a task

--- demo_app/models.py
+++ demo_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+STATUS = ((0, "Pending"), (1, "In progress"), (2, "Complete"), (3, "Late"))
+
+class Post(models.Model):
+    task_name = models.CharField(max_length=200, null=False, blank=False)
+    description = models.TextField()
+    user_id = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="task"
+    )
+    start_date = models.DateTimeField(null=False, blank=False)
+    start_time = models.DateTimeField(null=False, blank=False)
+    progress = models.IntegerField(null=False, blank=False)
+    status = models.IntegerField(choices=STATUS, default=0)
+    
+    
+
