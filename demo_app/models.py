44c221dd2467ffdf33d167dc68556aea1ac1ce0a
Oluwole Akani <oakani@gmail.com>
2024-08-02 13:05:49 +0000

    add task enabled in admin

--- demo_app/models.py
+++ demo_app/models.py
@@ -3,13 +3,12 @@ from django.contrib.auth.models import User
 
 STATUS = ((0, "Pending"), (1, "In progress"), (2, "Complete"), (3, "Late"))
 
-class Post(models.Model):
+class task(models.Model):
     task_name = models.CharField(max_length=200, null=False, blank=False)
-    description = models.TextField()
+    description = models.TextField(blank=True)
     user_id = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="task"
     )
-    start_date = models.DateTimeField(null=False, blank=False)
     start_time = models.DateTimeField(null=False, blank=False)
     progress = models.IntegerField(null=False, blank=False)
     status = models.IntegerField(choices=STATUS, default=0)
